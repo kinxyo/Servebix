641892bb62e68ac1aee268955345a42a0bd599d9
Kinjalk Tripathi <kinjalkt3@gmail.com>
2024-06-30 05:15:43 +0530

    View Setup

---
+++ views/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Baseline from "@/components/layers/baseline.vue";
+</script>
+
+<template>
+  <main class="dark">
+
+    <Baseline/>
+
+  </main>
+</template>
---
+++ views/vite.config.js
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  server: {
+    port: process.env.PORT || 3000,
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
